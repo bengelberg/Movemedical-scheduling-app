b98b9ffffce10c767b18b23bc055a94f577506e0
Ben Engelberg <benjaminsengelberg@gmail.com>
2022-12-13 12:56:35 -0800

    Updates

--- src/components/Alert.test.js
+++
@@ -1,6 +0,0 @@
-import { render } from "@testing-library/react";
-import Alert from "./Alert";
-
-test('If any form input fields are left empty, the no empty fields alert should be rendered', () => {
-    render(<Alert/>)
-})
